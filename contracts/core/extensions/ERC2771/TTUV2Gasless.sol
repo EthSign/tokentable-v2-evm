712e67909c4aae4f2b94b97c6ced25c57312fed4
boyuanx <boyuanxu@usc.edu>
2023-12-27 11:49:18 +0800

    Organize extensions

--- contracts/core/extensions/ERC2771/TTUV2Gasless.sol
+++ contracts/core/extensions/ERC2771/TTUV2Gasless.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.20;
 
 import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
-import {TokenTableUnlockerV2} from "../TokenTableUnlockerV2.sol";
-import {CustomERC2771Context} from "../../libraries/CustomERC2771Context.sol";
+import {TokenTableUnlockerV2} from "../../TokenTableUnlockerV2.sol";
+import {CustomERC2771Context} from "../../../libraries/CustomERC2771Context.sol";
 
 contract TTUV2Gasless is TokenTableUnlockerV2, CustomERC2771Context {
     function setTrustedForwarder(
